798c6edaa3e123fc732552b5689dd2028fb6a313
Caleb Lowe <caleb_lowe@live.com>
2018-02-10 12:49:36 -0500

    finished cyoa

--- CYOA/CYOA.js
+++ CYOA/CYOA.js
@@ -165,3 +165,18 @@ if(response === "deal the final blow"){
 } else if (response === "turn around") {
     response = prompt("you turn around to see a bloodied and furious shadow monster towering over you. it lets out a booming roar. This isnt over.    TO BE CONTINUED (never)");
 }
+
+
+} if(response === "continue down the path") {
+    response = prompt("as the evening sets in, you see a faint aura of light a ways ahead of you. you recognize this as the glow of a town.you have finally reached civilization. as the path ends, you hear something behind you.  | enter town | turn around | ");
+}  else if (response === "enter town") {
+    response = alert("youve finally made it out of the dark forest! as you enter town and look for help, you hear the furious roar of a wounded shadow monster entering behind you. This isnt over.   TO BE CONTINUED (never)");
+} else if (response === "turn around") {
+    response = alert("you turn around to see a bloodied and furious shadow monster towering over you. it lets out a booming roar. This isnt over.    TO BE CONTINUED (never)");
+}
+
+if(response === "enter town") {
+    response = alert("youve finally made it out of the dark forest! as you enter town and look for help, you hear the furious roar of a wounded shadow monster entering behind you. This isnt over.   TO BE CONTINUED (never)");
+} else if (response === "turn around") {
+    response = alert("you turn around to see a bloodied and furious shadow monster towering over you. it lets out a booming roar. This isnt over.    TO BE CONTINUED (never)");
+}
